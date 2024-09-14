f1e70d440fcefe2b44e84fb62de561f51e208b4a
noisyfood <2020302181169@whu.edu.cn>
2024-09-14 18:13:26 +0800

    first commit

---
+++ download.py
@@ -0,0 +1,4 @@
+
+huggingface-cli download --resume-download  moka-ai/m3e-base  --local-dir moka-ai/m3e-base
+
+huggingface-cli download --repo-type dataset --resume-download gretelai/synthetic_text_to_sql --local-dir gretelai/synthetic_text_to_sql
