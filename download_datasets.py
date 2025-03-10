f57e9ed27f5e885c39656490cbd33ccb46c191fb
YuwanZ <yuw4n0@gmail.com>
2025-03-10 03:20:14 +0000

    update

--- download_datasets.py
+++ download_datasets.py
@@ -1,1 +1,3 @@
+export HF_ENDPOINT=https://hf-mirror.com
 huggingface-cli download --repo-type dataset --resume-download gretelai/synthetic_text_to_sql --local-dir gretelai/synthetic_text_to_sql
+huggingface-cli download --repo-type dataset --resume-download yiting/UnsafeBench --local-dir yiting/UnsafeBench
