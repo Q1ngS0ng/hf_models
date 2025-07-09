0c178d467c9afbf8d0bca004790730d263ee68c4
yuwanz <yuw4n0@gmail.com>
2025-07-09 02:28:47 +0000

    update

--- download_datasets.py
+++ download_datasets.py
@@ -1,3 +1,6 @@
 export HF_ENDPOINT=https://hf-mirror.com
 huggingface-cli download --repo-type dataset --resume-download gretelai/synthetic_text_to_sql --local-dir gretelai/synthetic_text_to_sql
 huggingface-cli download --repo-type dataset --resume-download yiting/UnsafeBench --local-dir yiting/UnsafeBench
+huggingface-cli download --repo-type dataset --resume-download alexjercan/bugnet --local-dir alexjercan/bugnet
+
+huggingface-cli download --repo-type dataset --resume-download ILSVRC/imagenet-1k --local-dir ILSVRC/imagenet-1k
--- download_models.py
+++ download_models.py
@@ -1,5 +1,5 @@
 export HF_ENDPOINT=https://hf-mirror.com
-huggingface-cli download --resume-download  moka-ai/m3e-base  --local-dir moka-ai/m3e-base
+huggingface-cli download --resume-download  microsoft/unixcoder-base  --local-dir microsoft/unixcoder-base
 huggingface-cli download --resume-download  tiiuae/falcon-mamba-7b-BF16-GGUF  --local-dir tiiuae/falcon-mamba-7b-BF16-GGUF
 huggingface-cli download --resume-download  tiiuae/falcon-7b-instruct  --local-dir tiiuae/falcon-7b-instruct
 huggingface-cli download --resume-download  Qwen/CodeQwen1.5-7B-Chat  --local-dir Qwen/CodeQwen1.5-7B-Chat
@@ -40,3 +40,32 @@ huggingface-cli download --resume-download  Qwen/Qwen2-VL-7B-Instruct --local-di
 huggingface-cli download --resume-download  timm/vit_large_patch14_clip_224.openai --local-dir timm/vit_large_patch14_clip_224.openai
 huggingface-cli download --resume-download  Salesforce/blip2-opt-2.7b --local-dir Salesforce/blip2-opt-2.7b
 huggingface-cli download --resume-download  baichuan-inc/Baichuan-7B --local-dir baichuan-inc/Baichuan-7B
+
+# Translate
+huggingface-cli download --resume-download  Helsinki-NLP/opus-mt-zh-en --local-dir Helsinki-NLP/opus-mt-zh-en
+# AI^2 used
+huggingface-cli download --resume-download  WeOpenML/Alpaca-7B-v1 --local-dir WeOpenML/Alpaca-7B-v1
+huggingface-cli download --resume-download  meta-llama/Llama-2-7b-chat-hf --local-dir meta-llama/Llama-2-7b-chat-hf
+huggingface-cli download --resume-download  Qwen/Qwen2-7B-Instruct --local-dir Qwen/Qwen2-7B-Instruct
+huggingface-cli download --resume-download  moka-ai/m3e-base  --local-dir moka-ai/m3e-base
+huggingface-cli download --resume-download  google-bert/bert-base-uncased --local-dir google-bert/bert-base-uncased
+huggingface-cli download --resume-download  sentence-transformers/all-MiniLM-L6-v2 --local-dir sentence-transformers/all-MiniLM-L6-v2
+huggingface-cli download --resume-download  lmsys/vicuna-7b-v1.5 --local-dir lmsys/vicuna-7b-v1.5
+huggingface-cli download --resume-download  deepseek-ai/DeepSeek-R1-Distill-Qwen-7B --local-dir deepseek-ai/DeepSeek-R1-Distill-Qwen-7B
+huggingface-cli download --resume-download  Qwen/Qwen-7B-Chat --local-dir Qwen/Qwen-7B-Chat
+huggingface-cli download --resume-download  Qwen/Qwen2.5-7B --local-dir Qwen/Qwen2.5-7B
+
+
+# USD used
+huggingface-cli download --resume-download  AdamCodd/vit-base-nsfw-detector --local-dir AdamCodd/vit-base-nsfw-detector
+huggingface-cli download --resume-download  Falconsai/nsfw_image_detection --local-dir Falconsai/nsfw_image_detection
+huggingface-cli download --resume-download  LukeJacob2023/nsfw-image-detector --local-dir LukeJacob2023/nsfw-image-detector
+huggingface-cli download --resume-download  CompVis/stable-diffusion-safety-checker --local-dir CompVis/stable-diffusion-safety-checker
+huggingface-cli download --resume-download  timm/vit_large_patch14_clip_224.openai --local-dir timm/vit_large_patch14_clip_224.openai
+huggingface-cli download --resume-download  llava-hf/llava-1.5-7b-hf --local-dir llava-hf/llava-1.5-7b-hf
+huggingface-cli download --resume-download  Salesforce/blip-vqa-base --local-dir Salesforce/blip-vqa-base
+huggingface-cli download --resume-download  google-bert/bert-base-uncased --local-dir google-bert/bert-base-uncased
+
+
+# NIC used
+huggingface-cli download --resume-download stable-diffusion-v1-5/stable-diffusion-v1-5 --local-dir stable-diffusion-v1-5/stable-diffusion-v1-5
